902add07ed75110a3302c7d5897879e11a6f3cc6
Pedro Costa da Motta <79551989+PedroCMotta-oficial@users.noreply.github.com>
2025-02-18 16:04:47 -0300

    day one v2.0

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,18 +1,5 @@
-import { ArrowRight, Copy } from 'lucide-react';
-import {Button} from '@/components/button';
-import {IconButton} from '@/components/icon-button';
-
 export default function Home() {
   return (
-    <main>
-      <Button>
-        Enviar
-        <ArrowRight />
-      </Button>
-
-      <IconButton>
-        <Copy />
-      </IconButton>
-    </main>
+    <main></main>
   );
 }

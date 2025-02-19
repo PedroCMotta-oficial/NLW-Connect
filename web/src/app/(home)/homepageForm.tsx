dbf6cec3ce8577cabd8549762ae8628af0620fa0
Pedro Costa da Motta <79551989+PedroCMotta-oficial@users.noreply.github.com>
2025-02-19 12:20:36 -0300

    day two v4.0

--- web/src/app/(home)/homepageForm.tsx
+++ web/src/app/(home)/homepageForm.tsx
@@ -1,30 +1,69 @@
+"use client"
+
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { Button } from "@/components/button";
 import { InputField, InputIcon, InputRoot } from "@/components/input";
 import { ArrowRight, Mail, User } from "lucide-react";
 
+
+const subscriptionSchema = z.object({
+  name: z.string().min(2, "Digite seu nome completo"),
+  email: z.string().email("Digite um e-mail válido"),
+})
+
+type SubscriptionSchema = z.infer<typeof subscriptionSchema>
+
 export default function HomepageForm() {
+  const { register, handleSubmit, formState: { errors } } = useForm<SubscriptionSchema>({
+    resolver: zodResolver(subscriptionSchema),
+  })
+
+  function onSubscribe(data: SubscriptionSchema) {
+    console.log(data)
+  }
+
   return (
-    <form className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]">
+    <form
+      onSubmit={ handleSubmit(onSubscribe) }
+      className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]"
+    >
       <h2 className="font-heading font-semibold text-gray-200 text-xl">
         Inscrição
       </h2>
 
       <div className="space-y-3">
-        <InputRoot>
-          <InputIcon>
-            <User />
-          </InputIcon>
 
-          <InputField type="text" placeholder="Nome Completo" />
-        </InputRoot>
+        <div className="space-y-2">
+          <InputRoot>
+            <InputIcon>
+              <User />
+            </InputIcon>
 
-        <InputRoot>
-          <InputIcon>
-            <Mail />
-          </InputIcon>
+            <InputField type="text" placeholder="Nome Completo" { ...register("name") } />
+          </InputRoot>
 
-          <InputField type="email" placeholder="e-mail" />
-        </InputRoot>
+          { errors.name && (
+            <p className="text-danger text-xs font-semibold">{errors.name.message}</p>
+          ) }
+        </div>
+
+        <div className="space-y-2">
+          <InputRoot>
+            <InputIcon>
+              <Mail />
+            </InputIcon>
+
+            <InputField type="email" placeholder="e-mail" { ...register("email") } />
+          </InputRoot>
+
+          { errors.email && (
+            <p className="text-danger text-xs font-semibold">{errors.email.message}</p>
+          ) }
+        </div>
+
 
         <Button type="submit">
           Confirmar
